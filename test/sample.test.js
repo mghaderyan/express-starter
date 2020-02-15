f4571751f3d605bd0e62bf99a5aba44b18dceb98
Meysam Ghaderyan <mghaderyan@gmail.com>
2020-02-15 05:35:46 -0800

    WIP

--- test/sample.test.js
+++ test/sample.test.js
@@ -1,12 +1,19 @@
 
+describe('Array', function() {
+    describe('#indexOf()', function() {
+        it('should return -1 when the value is not present', function() {
+            expect([1, 2, 3].indexOf(5)).toBe(-1);
 
-test('should return -1 when the value is not present', () => {
-    expect([1, 2, 3].indexOf(5)).toBe(-1);
+        });
+    });
 });
 
-test('responds with matching records', async () => {
-    const myPromise = Promise.resolve(3);
+describe('#async()', function() {
+    it('responds with matching records', async function() {
+        const myPromise = Promise.resolve(3);
 
-    const users = await myPromise;
-    expect(users).toBe(3);
+        const users = await myPromise
+        expect(users).toBe(3);
+
+    });
 });
